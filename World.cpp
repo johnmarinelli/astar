eb573fd7ecbf6507905ef1fcafd4a67e0c63c99e
john <jonmarinelli@hotmail.com>
2014-08-03 22:42:46 -0400

    refactored, need to check when exactly to calculate f, g, h vals

--- World.cpp
+++ World.cpp
@@ -11,7 +11,7 @@ bool isInList(const GridCell* cell, const std::vector<GridCell*>& list)
 }
 
 /*
-* This function requires the beginning cell, the destination cell, and the cell being inspected
+* Requires the beginning cell, the destination cell, and the cell being inspected
 * Sets "cell"s hval, fval, and gval
 * returns "cell"s gval
 */
@@ -32,6 +32,62 @@ int getGValue(GridCell* const start, GridCell* const end, GridCell* cell)
 	cell->mFValue = fval;
 
 	return gval;
+}
+
+/*
+* Requires open list
+* returns cell with lowest fval
+*/
+GridCell* getLowestFValCell(const std::vector<GridCell*>& list)
+{
+	//placeholders
+	GridCell* lowest_fval_cell = list.front();
+	int placeholder_fval = lowest_fval_cell->mFValue;
+
+	//go in reverse order 
+	for(auto itr = list.rbegin(); itr != list.rend(); ++itr){
+		auto cell = *itr;
+		int fval = cell->mFValue;
+
+		if(fval < placeholder_fval){
+			placeholder_fval = fval;
+			lowest_fval_cell = cell;
+		}
+	}
+
+	return lowest_fval_cell;
+}
+
+/*
+* Requires cell to move, the beginning list, and destination list
+*/
+void switchLists(GridCell* cell, std::vector<GridCell*>& original_list, std::vector<GridCell*>& destination_list)
+{
+	//remove starting node from original list, add it to destination
+	original_list.erase(std::remove(original_list.begin(), original_list.end(), cell), original_list.end());
+	destination_list.push_back(cell);
+}
+
+int populateOpenList(GridCell* parent, GridCell* destination, std::vector<GridCell*>& open_list, std::vector<GridCell*>& closed_list)
+{
+	for(auto cell : parent->getNeighbors()){
+		if(cell->getWalkable() && cell != parent && !isInList(cell, closed_list)){
+			//check if cell is in open list already
+			if(isInList(cell, open_list)){
+				//wut?????? if x + y > x??
+				int new_gval = cell->mGValue + parent->mGValue;
+				if(new_gval < parent->mGValue){
+					cell->mParent = parent;
+					getGValue(parent, destination, cell);
+				}
+			}
+			else{
+				open_list.push_back(cell);
+				getGValue(parent, destination, cell);
+				cell->mParent = parent;
+			}
+		}
+	}
 }
 
 World::World(sf::RenderWindow& window) : mRenderTarget(&window)
@@ -71,71 +127,31 @@ void World::update(float dTime)
 	openlist.push_back(mBeginNode);
 	
 	//add walkable nodes to open list
-	for(auto cell : mBeginNode->getNeighbors()){
-		if(cell->getWalkable() && cell != mBeginNode){
-			openlist.push_back(cell);
-			cell->mParent = mBeginNode;
-		}
-	}
+	populateOpenList(mBeginNode, mEndNode, openlist, closedlist);
 	
 	//remove starting node from open list, add it to closed list
-	openlist.erase(std::remove(openlist.begin(), openlist.end(), mBeginNode), openlist.end());
-	closedlist.push_back(mBeginNode);
+	switchLists(mBeginNode, openlist, closedlist);
 
 	//calculate g, h, and f values for each cell
 	for(auto cell : openlist){
-/*		int gval = 10, hval = 0, fval = 0;
-		cell->mGValue = 10;
-
-		if(mBeginNode->isDiagonalNeighbor(cell)){
-			gval = 14;
-			cell->mGValue = gval;
-		}
-		
-		hval = getManhattanDistance(cell->getArrayCoords().x, cell->getArrayCoords().y, mEndNode->getArrayCoords().x, mEndNode->getArrayCoords().y) * 10;
-		cell->mHValue = hval;
-
-		fval = hval + gval;
-		cell->mFValue = fval;
-*/
 		getGValue(mBeginNode, mEndNode, cell);
 	}
 
-	GridCell* lowest_fval_cell = openlist.front();
-	int placeholder_fval = lowest_fval_cell->mFValue;
-
-	//drop lowest f value cell
-	for(auto cell : openlist){
-		int fval = cell->mFValue;
-
-		if(fval < placeholder_fval){
-			placeholder_fval = fval;
-			lowest_fval_cell = cell;
-		}
-	}
+	//get lowest fval cell
+	GridCell* lowest_fval_cell = getLowestFValCell(openlist);
 
 	//remove lowest fval cell from openlist & put in closed list
-	openlist.erase(std::remove(openlist.begin(), openlist.end(), lowest_fval_cell));
-	closedlist.push_back(lowest_fval_cell);
+	switchLists(lowest_fval_cell, openlist, closedlist);
 
-	//add walkable nodes to open list
-	for(auto cell : lowest_fval_cell->getNeighbors()){
-		if(cell->getWalkable() && cell != lowest_fval_cell && !isInList(cell, closedlist)){
-			//check if the cell is in open list already
-			if(isInList(cell, openlist)){
-				int new_g_val = cell->mGValue + lowest_fval_cell->mGValue;
-				if(new_g_val < lowest_fval_cell->mGValue){
-					cell->mParent = lowest_fval_cell;
-					getGValue(lowest_fval_cell, mEndNode, cell);
-				}
-			}
-			else{
-				openlist.push_back(cell);
-				cell->mParent = lowest_fval_cell;
-			}
-		}
-	}
-	
+	//add walkable nodes next to lowest_fval_cell to open list
+	populateOpenList(lowest_fval_cell, mEndNode, openlist, closedlist);
+
+	//get lowest fval cell
+	lowest_fval_cell = getLowestFValCell(openlist);
+
+	switchLists(lowest_fval_cell, openlist, closedlist);
+
+	populateOpenList(lowest_fval_cell, mEndNode, openlist, closedlist);
 }
 
 void World::render()
